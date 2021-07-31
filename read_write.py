7b998b713f520030ce36d0e756f0713f3d07fad6
RAVI VARMA B <87261497+ravi21bala@users.noreply.github.com>
2021-07-31 17:15:52 +0530

    read

---
+++ read_write.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 31 16:28:46 2021
+
+@author: Ravivarma
+"""
+
+#-------------------------Reading & Writing data in Files----------------------
+
+import pandas
+
+# Reading CSV Files with Pandas:
+df = pandas.read_csv('G:/python course/User_Data.csv')
+print(df)
+
+# Writing CSV Files with Pandas:
+df.to_csv('G:/python course/IIT-B.csv')
+
+# Reading Excel Files with Pandas
+df1 = pandas.read_excel('G:/python course/User_Data.xlsx')
+
+df1 = pandas.read_excel('User_Data.xlsx')
+print(df1)
+
+# Writing Excel Files with Pandas 
+df1.to_excel('IIT-B.xlsx')
+df2 = pandas.DataFrame(df1)
+print (df2)
